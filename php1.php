d39ee9b490681df664dc56f344eb0e9ba9443e11
Filipas <filip@robotikosakademija.lt>
2017-12-18 11:38:13 +0200

    Namų darbai bandymai su funkcijomis

--- php1.php
+++ php1.php
@@ -1,5 +1,18 @@
-<?php
 
-Labasg
+
+<?php
+$a = array(5, 6, 10, 15);
+$b =array(8,5, 3, 25);
+
+
+$a = array_filter($a);
+$average = array_sum($a)/count($a);
+
+ 
+$b = array_filter($b);
+$average1 = array_sum($b)/count($b);
+
+echo ($average1 - $average);
+
 
 ?>
