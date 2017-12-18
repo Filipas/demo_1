bdf9072965119cdd3539da0e495e658e96bab645
Filipas <filip@robotikosakademija.lt>
2017-12-18 11:52:56 +0200

    Bandymas 2 funckijos

--- php1.php
+++ php1.php
@@ -1,10 +1,11 @@
 
 <?php
 
-//Naudojamos vidinės funkcijos
+
 $a = array(5, 6, 10, 15);
 $b =array(8,5, 3, 25);
 
+//Naudojamos vidinės funkcijos
 
 $a = array_filter($a);
 $average = array_sum($a)/count($a);
@@ -15,6 +16,15 @@ $average1 = array_sum($b)/count($b);
 
 echo ($average1 - $average);
 
+
+function vidurkis($a) {
+ return array_sum($a) / count($a);
+}
+function vidurkis2($b) {
+ return array_sum($b) / count($b);
+}
+
+echo (vidurkis($a) - vidurkis2($b));
 
 
 ?>
